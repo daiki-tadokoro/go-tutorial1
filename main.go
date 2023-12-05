cd56674df9414f38360f88fd63709c9ab776d750
daiki-tadokoro <>
2023-12-06 08:56:00 +0900

    feat: 明示的な宣言

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-	fmt.Println(time.Now())
+	// 明示的な定義
+	// var 変数名 型 = 値
+	var i int = 100
+
+	fmt.Println(i) // 100
+
+	var s string = "Hello World"
+	fmt.Println(s) // Hello World
+
+	var t, f bool = true, false
+	fmt.Println(t, f) // true false
+
+	var (
+		i2 int    = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2) // 200 Golang
+
+	var i3 int
+	var s3 string
+
+	fmt.Println(i3, s3) // 0
+
+	i3 = 300
+	s3 = "Go"
+	fmt.Println(i3, s3) // 300 Go
 }
