0220e72c767ecd24a140817297671128bb7398e0
daiki-tadokoro <>
2023-12-06 09:04:31 +0900

    feat: 変数の定義

--- main.go
+++ main.go
@@ -2,31 +2,29 @@ package main
 
 import "fmt"
 
+func outer() {
+	var s4 string = "outer"
+	fmt.Println(s4)
+}
+
 func main() {
-	// 明示的な定義
-	// var 変数名 型 = 値
-	var i int = 100
+	// 暗黙的な定義(型推論)
+	// 変数名 := 値
+	i4 := 400
+	fmt.Println(i4) // 400
 
-	fmt.Println(i) // 100
+	// 値の更新
+	i4 = 450
+	fmt.Println(i4) // 450
 
-	var s string = "Hello World"
-	fmt.Println(s) // Hello World
+	// 再定義はできない
+	// i4 := 500
+	// fmt.Println(i4)
 
-	var t, f bool = true, false
-	fmt.Println(t, f) // true false
+	// 型の違う値を代入することはできない
+	// i4 = "Hello"
+	// fmt.Println(i4)
 
-	var (
-		i2 int    = 200
-		s2 string = "Golang"
-	)
-	fmt.Println(i2, s2) // 200 Golang
+	outer()
 
-	var i3 int
-	var s3 string
-
-	fmt.Println(i3, s3) // 0
-
-	i3 = 300
-	s3 = "Go"
-	fmt.Println(i3, s3) // 300 Go
 }
