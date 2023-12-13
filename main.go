d06616d6e70840d0ceb0eaa4da4253009da30bf7
daiki-tadokoro <>
2023-12-14 08:48:11 +0900

    feat: 文字列型の作成

--- main.go
+++ main.go
@@ -3,9 +3,31 @@ package main
 import "fmt"
 
 // 型
-// 論理値型
+// 文字列型
 
 func main() {
-	var t, f bool = true, false
-	fmt.Println(t, f)
+	var s string = "Hello Golang"
+	fmt.Printf("%T\n", s) // string
+
+	var s1 string = "300"
+	fmt.Printf("%T\n", s1) // string
+
+	// 複数行の文字列
+	var s2 string = `Hello
+	Golang
+	!`
+	fmt.Println(s2)
+	fmt.Printf("%T\n", s2) // string
+
+	// ダブルクォーテーションを文字列として扱う
+	var s3 string = "\""
+	fmt.Println(s3) // "
+
+	var s4 string = `"`
+	fmt.Println(s4) // "
+
+	// 文字列の長さ
+	fmt.Println(s[0])         // 72
+	fmt.Println(string(s[0])) // H
+
 }
