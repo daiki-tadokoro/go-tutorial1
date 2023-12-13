e84e5cc5ac0cea811259a575a8f5563ea72031c4
daiki-tadokoro <>
2023-12-14 08:40:45 +0900

    feat: 論理値型の作成

--- main.go
+++ main.go
@@ -3,29 +3,9 @@ package main
 import "fmt"
 
 // 型
-// 浮動小数点型
+// 論理値型
 
 func main() {
-	var fl64 float64 = 2.4
-	fmt.Println(fl64)
-
-	fl := 3.2
-	fmt.Println(fl64 + fl)
-	fmt.Printf("%T, %T\n", fl64, fl)
-
-	var fl32 float32 = 1.2
-	fmt.Printf("%T\n", fl32) // float32
-
-	// 型変換
-	fmt.Printf("%T\n", float64(fl32)) // float64
-
-	zero := 0.0
-	pinf := 1.0 / zero
-	fmt.Println(pinf) // +Inf
-
-	ninf := -1.0 / zero
-	fmt.Println(ninf) // -Inf
-
-	nan := zero / zero
-	fmt.Println(nan) // NaN
+	var t, f bool = true, false
+	fmt.Println(t, f)
 }
